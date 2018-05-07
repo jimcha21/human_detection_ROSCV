8c411d42b92980b079ece5aaf8649af66f117123
jimcha21 <dchatziparaschis@gmail.com>
2018-05-07 22:18:14 +0300

    changed some names

--- guidance/src/allCamerasSonars_save.cpp
+++ guidance/src/allCamerasSonars_save.cpp
@@ -419,16 +419,16 @@ int main(int argc, char** argv)
   range_pubs[4] = my_node.advertise<sensor_msgs::Range>("/guidance/left/ultrasonic",1);
 
 	//frame names
-  images[0].header.frame_id = "guidanceDown_leftcamera_link";
-  images[1].header.frame_id = "guidanceDown_rightcamera_link";
-  images[2].header.frame_id = "guidanceFront_leftcamera_link";
-  images[3].header.frame_id = "guidanceFront_rightcamera_link";
-  images[4].header.frame_id = "guidanceRight_leftcamera_link";
-  images[5].header.frame_id = "guidanceRight_rightcamera_link";
-  images[6].header.frame_id = "guidanceRear_leftcamera_link";
-  images[7].header.frame_id = "guidanceRear_rightcamera_link";
-  images[8].header.frame_id = "guidanceLeft_leftcamera_link";
-  images[9].header.frame_id = "guidanceLeft_rightcamera_link";
+  images[0].header.frame_id = "guidanceDown_leftcamera_frame";
+  images[1].header.frame_id = "guidanceDown_rightcamera_frame";
+  images[2].header.frame_id = "guidanceFront_leftcamera_frame";
+  images[3].header.frame_id = "guidanceFront_rightcamera_frame";
+  images[4].header.frame_id = "guidanceRight_leftcamera_frame";
+  images[5].header.frame_id = "guidanceRight_rightcamera_frame";
+  images[6].header.frame_id = "guidanceRear_leftcamera_frame";
+  images[7].header.frame_id = "guidanceRear_rightcamera_frame";
+  images[8].header.frame_id = "guidanceLeft_leftcamera_frame";
+  images[9].header.frame_id = "guidanceLeft_rightcamera_frame";
 
   ranges[0].header.frame_id = "ultrasonicDown_link";
   ranges[1].header.frame_id = "ultrasonicFront_link";
@@ -500,7 +500,7 @@ int main(int argc, char** argv)
   RETURN_IF_ERR(err_code);
 
   std::cout << "start_transfer" << std::endl;	
-  ros::Timer timer = my_node.createTimer(ros::Duration(1), postTf_sch_); //Posting tf info in fixed time - 10Hz
+  ros::Timer timer = my_node.createTimer(ros::Duration(10), postTf_sch_); //Posting tf info in fixed time - 10Hz
 
 	while (ros::ok())
   {
