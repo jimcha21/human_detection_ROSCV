95355f8e2a8d5972c5b455951d0671e407fcd53e
jimcha21 <dchatziparaschis@gmail.com>
2018-09-20 20:50:35 +0300

    made some adds

--- guidance/nodes/laserscan_transform_toNao.cpp
+++ guidance/nodes/laserscan_transform_toNao.cpp
@@ -1,10 +1,12 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Point.h"
 #include "stdio.h"
 #include <tf/transform_listener.h>
+#include "math.h"
 
-
-sensor_msgs::LaserScan laserscan_;
+sensor_msgs::LaserScan laserscan_,naolaser_;
+bool got_laser = false;
 
 /*std_msgs/Header header
   uint32 seq
@@ -23,8 +25,11 @@ sensor_msgs::LaserScan laserscan_;
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  laserscan_ = *msg;
-  std::cout << laserscan_.header.frame_id << std::endl;
+  laserscan_ = naolaser_ = *msg;
+  naolaser_.header.frame_id = "ar_marker_4";
+  std::cout << laserscan_.ranges.size() << std::endl;
+  got_laser=true;
+
 }
 
 int main(int argc, char **argv)
@@ -34,22 +39,33 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("/scan", 1, laserCallback);
-
+  ros::Publisher pub = n.advertise<sensor_msgs::LaserScan>("nao_scan", 1);
 
   tf::TransformListener listener;
   ros::Rate l(40);
+
   while(ros::ok){
 
-    tf::StampedTransform transform;
+    if(!got_laser)
+    {
+      l.sleep();
+      ros::spinOnce();
+      continue;
+    }      
+
+/*    tf::StampedTransform mapTolaser_tf,mapToarmarker_tf;
     try{
-      listener.lookupTransform("/map", "/laser", ros::Time(0), transform);
-      listener.lookupTransform("/map", "/guidanceDown_leftcamera_opticalframe", ros::Time(0), transform);
+      listener.lookupTransform("/map", "/laser", ros::Time(0), mapTolaser_tf);
+      listener.lookupTransform("/map", "/guidanceDown_leftcamera_opticalframe", ros::Time(0), mapToarmarker_tf);
     }
     catch (tf::TransformException ex){
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
-    }
+    }*/
 
+
+
+     pub.publish(naolaser_);
 
     l.sleep();
     ros::spinOnce();
